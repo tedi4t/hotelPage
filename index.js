8d3e052cb0c8fd85f22d14a52dfef73ebf820ad6
dmytro <tedi4t@gmail.com>
2021-02-07 00:30:17 +0200

    improved navbar

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const innerHeight = $(window).innerHeight();
 
 $(window).scroll(() => {
-  const offsetHeight = $(document).scrollTop() + innerHeight * 0.5;
+  const scrollTop = $(document).scrollTop();
+  const offsetHeight = scrollTop + innerHeight * 0.5;
+
   $('.img-top').map((ind, elem) => {
     const elementHeight = $(elem).innerHeight()
     const itemOffsetHeight = $(elem).offset().top + elementHeight * 0.5;
@@ -13,4 +15,14 @@ $(window).scroll(() => {
     const itemOffsetHeight = $(elem).offset().top + elementHeight * 0.5;
     $( elem ).css( "transform", `translateY(${ - (itemOffsetHeight - offsetHeight) / 6 + 80 }px)` );
   });
+
+  if (scrollTop > 0) {
+    $('#navbar').css('background', '#fff');
+    $('#navbar').css('box-shadow', '0 0 15px rgba(0, 0, 0, 0.8)');
+    $('.nav-link').css('color', '#000');
+  } else {
+    $('#navbar').css('background', 'rgba(0, 0, 0, 0)');
+    $('#navbar').css('box-shadow', 'none');
+    $('.nav-link').css('color', '#fff');
+  }
 })
