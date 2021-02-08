4d639b1ba030bab69b794112c466583d1feb091c
dmytro <tedi4t@gmail.com>
2021-02-08 15:23:03 +0200

    add animation on scrolling

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const innerHeight = $(window).innerHeight();
+const sections = $('.animate-block');
+sections.map((ind, elem) => {
+  $(elem).addClass('before-animation');
+})
 
 $(window).scroll(() => {
   const scrollTop = $(document).scrollTop();
+  const scrollBottom = scrollTop + innerHeight;
   const offsetHeight = scrollTop + innerHeight * 0.5;
+
+  sections.map((ind, elem) => {
+    if (elem) {
+      const itemOffsetHeight = $(elem).offset().top;
+      if (itemOffsetHeight + 100 < scrollBottom) {
+        $(elem).addClass('animate');
+        sections.splice(ind, 1);
+      }
+    }
+  })
 
   $('.img-top').map((ind, elem) => {
     const elementHeight = $(elem).innerHeight()
